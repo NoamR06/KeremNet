d622971b6bf1cbdcb7f3f709ad1e3e9234b7b87e
NoamR06 <noamreshef2006@gmail.com>
2025-06-23 16:41:37 +0300

    Added css to the posts & comments

--- kerem-net/src/App.tsx
+++ kerem-net/src/App.tsx
@@ -5,6 +5,30 @@ import { PostComponent } from './Features/Post/PostComponent';
 
 function App() {
   return (
+    <div className="posts-container">
+      <PostComponent post={
+      {
+        id: '1',
+        author: 'Noam Reshef',
+        content: 'This is a simple Kerem Content Post.',
+        date: new Date(),
+        likes: 0,
+        comments: [
+          {
+            id: '1',
+            author: 'Zoee Zebra',
+            content: 'This is a comment on the post.',
+            date: new Date(),
+          },
+          {
+            id: '2',
+            author: 'Master Shifo',
+            content: 'This is another comment on the post.',
+            date: new Date(),
+          },
+        ],
+    }}></PostComponent>
+    
     <PostComponent post={
       {
         id: '1',
@@ -27,6 +51,129 @@ function App() {
           },
         ],
     }}></PostComponent>
+    
+
+    <PostComponent post={
+      {
+        id: '1',
+        author: 'Noam Reshef',
+        content: 'This is a simple Kerem Content Post.',
+        date: new Date(),
+        likes: 0,
+        comments: [
+          {
+            id: '1',
+            author: 'Zoee Zebra',
+            content: 'This is a comment on the post.',
+            date: new Date(),
+          },
+          {
+            id: '2',
+            author: 'Master Shifo',
+            content: 'This is another comment on the post.',
+            date: new Date(),
+          },
+        ],
+    }}></PostComponent>
+
+    
+    <PostComponent post={
+      {
+        id: '1',
+        author: 'Noam Reshef',
+        content: 'This is a simple Kerem Content Post.',
+        date: new Date(),
+        likes: 0,
+        comments: [
+          {
+            id: '1',
+            author: 'Zoee Zebra',
+            content: 'This is a comment on the post.',
+            date: new Date(),
+          },
+          {
+            id: '2',
+            author: 'Master Shifo',
+            content: 'This is another comment on the post.',
+            date: new Date(),
+          },
+        ],
+    }}></PostComponent>
+
+    
+    <PostComponent post={
+      {
+        id: '1',
+        author: 'Noam Reshef',
+        content: 'This is a simple Kerem Content Post.',
+        date: new Date(),
+        likes: 0,
+        comments: [
+          {
+            id: '1',
+            author: 'Zoee Zebra',
+            content: 'This is a comment on the post.',
+            date: new Date(),
+          },
+          {
+            id: '2',
+            author: 'Master Shifo',
+            content: 'This is another comment on the post.',
+            date: new Date(),
+          },
+        ],
+    }}></PostComponent>
+
+    
+    <PostComponent post={
+      {
+        id: '1',
+        author: 'Noam Reshef',
+        content: 'This is a simple Kerem Content Post.',
+        date: new Date(),
+        likes: 0,
+        comments: [
+          {
+            id: '1',
+            author: 'Zoee Zebra',
+            content: 'This is a comment on the post.',
+            date: new Date(),
+          },
+          {
+            id: '2',
+            author: 'Master Shifo',
+            content: 'This is another comment on the post.',
+            date: new Date(),
+          },
+        ],
+    }}></PostComponent>
+
+    
+    <PostComponent post={
+      {
+        id: '1',
+        author: 'Noam Reshef',
+        content: 'This is a simple Kerem Content Post.',
+        date: new Date(),
+        likes: 0,
+        comments: [
+          {
+            id: '1',
+            author: 'Zoee Zebra',
+            content: 'This is a comment on the post.',
+            date: new Date(),
+          },
+          {
+            id: '2',
+            author: 'Master Shifo',
+            content: 'This is another comment on the post.',
+            date: new Date(),
+          },
+        ],
+    }}></PostComponent>
+
+    </div>
+    
   )
 };
 export default App;
--- kerem-net/src/Features/Post/PostComponent.tsx
+++ kerem-net/src/Features/Post/PostComponent.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { v4 as uuidv4 } from 'uuid';
+import './PostComponent.css';
 
 export interface Comment {
   id: string;
