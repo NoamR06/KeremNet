7956f458588cd8edbaeda9ceb7e3cab45fe762e3
NoamR06 <noamreshef2006@gmail.com>
2025-06-24 19:21:39 +0300

    Moved input boxes & add comment button to a different component

--- kerem-net/src/Features/Post/CommentComponent/AddCommentButton/AddCommentButton.tsx
+++ kerem-net/src/Features/Post/CommentComponent/AddCommentButton/AddCommentButton.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./AddCommentButton.css"
+import { argv } from 'process';
 
 interface AddCommentButtonProps {
-  onButtonClick: () => void;
+  onButtonClick: (inputUsername: string, inputContent: string) => void;
 }
 
-export const AddCommentButton: React.FC<AddCommentButtonProps> = ({ onButtonClick }) => {
+export const AddCommentButton: React.FC<AddCommentButtonProps> = ({ onButtonClick}) => {
+    const [inputUsername, setInputUsername] = useState<string>('');
+    const [inputContent, setInputContent] = useState<string>('');
+  
+    const handleInputUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setInputUsername(event.target.value);
+    };
+  
+    const handleInputContentChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setInputContent(event.target.value);
+    };
+
     return (
     <div>
-        <button id="post_add_comment_button" onClick={onButtonClick}>
+        <input type="text" value={inputUsername} onChange={handleInputUsernameChange} id="post_add_comment_name" 
+        placeholder="User name" />
+        <input type="text" value={inputContent} onChange={handleInputContentChange} id="post_add_comment_content" 
+        placeholder="Comment content" />
+        <button id="post_add_comment_button" onClick={() => onButtonClick(inputUsername.toString(), inputContent.toString())}>
           Add Comment
         </button>
     </div>
--- kerem-net/src/Features/Post/CommentComponent/CommentsSection/CommentsSection.tsx
+++ kerem-net/src/Features/Post/CommentComponent/CommentsSection/CommentsSection.tsx
@@ -4,7 +4,6 @@ import "./CommentsSection.css";
 import { v4 as uuidv4 } from 'uuid';
 import { AddCommentButton } from '../AddCommentButton/AddCommentButton';
 import { UserComments } from '../UserComments/UserComments';
-import { CommentInput } from '../CommentInput/CommentInput';
 
 export interface CommentsProps {
   post_comments: Comment[];
@@ -12,18 +11,8 @@ export interface CommentsProps {
 
 export const CommentsSection: React.FC<CommentsProps> = ({ post_comments }) => {
     const [comments, setComments] = useState(post_comments);
-    const [inputUsername, setInputUsername] = useState<string>('');
-    const [inputContent, setInputContent] = useState<string>('');
   
-    const handleInputUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      setInputUsername(event.target.value);
-    };
-  
-    const handleInputContentChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      setInputContent(event.target.value);
-    };
-  
-    const handleAddComment = () => {
+    const handleAddComment = (inputUsername: string, inputContent: string) => {
       setComments([...comments, ({
         id: uuidv4(),
         author: inputUsername || "Anonymous",
@@ -36,8 +25,6 @@ export const CommentsSection: React.FC<CommentsProps> = ({ post_comments }) => {
     <div className='comments'>
         <h2>Comments</h2> 
         <UserComments post_comments={comments} />
-        <CommentInput InputUsernameValue={inputUsername} InputContentValue={inputContent} 
-        handleUsernameInput={handleInputUsernameChange} handleContentInput={handleInputContentChange}/>
         <AddCommentButton onButtonClick={handleAddComment}/>
     </div>
   )
