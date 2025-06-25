05638bea9ce94c7754dc30a58deed0ec7a89d790
NoamR06 <noamreshef2006@gmail.com>
2025-06-25 14:26:39 +0300

    Added a single post server get request

--- ServerSide/src/Server.ts
+++ ServerSide/src/Server.ts
@@ -1,11 +1,23 @@
 import express, { Request, Response } from 'express';
+const Posts: { id:number, author: string, content: string, date: string, likes: number, comments: [] }[] = require('../db.json');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Normal test');
+app.get('/posts', (req: Request, res: Response) => {
+    res.json(Posts);
 });
+
+app.get('/posts/:id', (req: Request, res: Response) => {
+    const objectId = parseInt(req.params.id);
+    const post = Posts.find((post) => post.id === objectId);
+    if (post) {
+        res.json(post);
+      } else {
+        res.status(404).json({ message: 'Post not found' });
+      }
+});
+
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
