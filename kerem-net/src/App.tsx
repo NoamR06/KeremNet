5c39fb706d2deee38448ac27e43fb8cd5aa2f6b3
NoamR06 <noamreshef2006@gmail.com>
2025-06-25 11:55:06 +0300

    Made the 3 components of the page be a grid

--- kerem-net/src/App.tsx
+++ kerem-net/src/App.tsx
@@ -1,14 +1,12 @@
 import React, { Fragment } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './Components/Pages/HomePage';
-import ApplicationLayout from './Components/ApplicationLayout/ApplicationLayout';
 
 
 
 const App: React.FC = () => {
   return (
     <Fragment>
-      <ApplicationLayout />
       <Routes>
         <Route path="/" element={<HomePage />} />
       </Routes>
--- kerem-net/src/Components/Pages/HomePage.tsx
+++ kerem-net/src/Components/Pages/HomePage.tsx
@@ -1,13 +1,14 @@
 import Footer from "../ApplicationLayout/Footer/Footer";
 import Navbar from "../ApplicationLayout/NavigationBar/navigationBar";
 import { Posts } from "../Post/Post";
+import "./HomePage.css"
 
 const HomePage: React.FC = () => {
   return (
-    <div>
-      <Navbar />
-      <Posts />
-      <Footer />
+    <div className="grid-container">
+      <div className="top-section "><Navbar /></div>
+      <div className="middle-section"><Posts /><Posts /><Posts /><Posts /></div>
+      <div className="bottom-section"><Footer /></div>
     </div>
   );
 };
