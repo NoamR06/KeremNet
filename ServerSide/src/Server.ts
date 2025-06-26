ae358d358b31e2e17e6013d4b35672be9097ffd2
NoamR06 <noamreshef2006@gmail.com>
2025-06-26 11:03:22 +0300

    Fixed CORS issue

--- ServerSide/src/Server.ts
+++ ServerSide/src/Server.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import PostRoutes from './Routes/PostsRoutes'
 import ApplicationRoutes from './Routes/ApplicationRoutes'
-
+import cors from 'cors'
 const app = express();
 app.use(express.json());
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3002;
+
+app.use(cors({
+    origin: '*'
+}))
 
 app.use('/posts', PostRoutes)
 app.use('/application', ApplicationRoutes)
@@ -12,3 +16,6 @@ app.use('/application', ApplicationRoutes)
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });
+
+//Fix 2 package.json
+//Fix x2 posts
