9a324ec9c252f44a84b7b10a622cb5d5c74bbcbe
NoamR06 <noamreshef2006@gmail.com>
2025-06-27 11:06:43 +0300

    Moved css to the correct spot

--- ReactSide/src/Components/PostCreation/PostCreation.tsx
+++ ReactSide/src/Components/PostCreation/PostCreation.tsx
@@ -8,8 +8,8 @@ const PostCreation: React.FC = () => {
   const [error, setError] = useState<string | null>(null);
   const [success_message, setSuccessMessage] = useState<Boolean>(false);
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
+  const handleSubmit = async (post_creation_event: React.FormEvent) => {
+    post_creation_event.preventDefault();
     setSuccessMessage(true);
 
     setTimeout(() => setSuccessMessage(false), 5000);
@@ -44,21 +44,21 @@ const PostCreation: React.FC = () => {
   };
   if (error) return <p> <ErrorComponent received_errors={error} /> </p>;
   return (
-<div className="post_creation">
-           <h1>Create a New Post</h1>
-          <form id="post-creator-form">
-              <div className="form-group">
-                  <p>Your Name</p>
-                  <input type="text" id="author-name-input" placeholder="Write your name here." 
-                  value={post_author} onChange={(text) => setAuthor(text.target.value)}/>
-                  <p>PostContent</p>
-                  <textarea id="post-content-input" rows={4} placeholder="Write your post content here." 
-                  required value={post_content} onChange={(text) => setContent(text.target.value)} />
-              </div>
-              <button type="submit" id="create-post-btn" onClick={handleSubmit}>Post</button>
-              {success_message && <p id="success_message">Post was successfully created!</p>}
-          </form>
-        </div>
+    <div className="post_creation">
+        <h1>Create a New Post</h1>
+        <form id="post-creator-form">
+            <div className="form-group">
+                <p>Your Name</p>
+                <input type="text" id="author-name-input" placeholder="Write your name here." 
+                value={post_author} onChange={(text) => setAuthor(text.target.value)}/>
+                <p>PostContent</p>
+                <textarea id="post-content-input" rows={4} placeholder="Write your post content here." 
+                required value={post_content} onChange={(text) => setContent(text.target.value)} />
+            </div>
+            <button type="submit" id="create-post-btn" onClick={handleSubmit}>Post</button>
+            {success_message && <p id="success_message">Post was successfully created!</p>}
+        </form>
+    </div>
   );
 };
 export default PostCreation;
